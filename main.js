16cc7892af3919b57f6aea974419c095bed6cbc2
Mayran Adriel <mayranalbuquerque@gmail.com>
2024-03-23 23:07:06 -0300

    primeiro prototipo finalizado

--- main.js
+++ main.js
@@ -1,31 +1,59 @@
-let vetor = [];
-
+let tarefasNaoConcluidas = [];
+let tarefasConcluidas = [];
 let divTarefas = document.querySelector("#listTasks");
 
-function pegandoValor() {
-    let elemento = document.querySelector("#taskInput").value;
-    if (elemento === "") {
-        alert("Digite uma tarefa!")
-    } else {
-        vetor.push(elemento);
-        divTarefas.innerHTML = "";
-        for (let i = 0; i < vetor.length; i++) {
-            divTarefas.innerHTML += `<div class="tarefa">
-            <h3>${vetor[i]}</h3>
-            <div>
-                <button onclick="marcarConcluido(${i})">Concluida</button>
-                <button onclick="limparTarefa(${i})">Excluir</button>
-            </div>
+let estadoTarefa = "tarefaNaoConcluida";
+let tarefaFoiConcluida = false;
+
+function verificaDuplicidadeInputVazio() {
+  let valorInput = document.querySelector("#taskInput").value;
+  if (
+    tarefasNaoConcluidas.includes(valorInput) ||
+    tarefasConcluidas.includes(valorInput)
+  ) {
+    alert("Tarefa já cadastrada!");
+  } else if (valorInput === "") {
+    alert("Digite uma tarefa!");
+  } else {
+    tarefasNaoConcluidas.push(valorInput);
+    imprimirLista(tarefasNaoConcluidas);
+  }
+}
+
+function deletarTarefa(indice, concluida) {
+  if (concluida) {
+    tarefasConcluidas.splice(indice, 1);
+    imprimirLista(tarefasConcluidas);
+  } else {
+    tarefasNaoConcluidas.splice(indice, 1);
+    imprimirLista(tarefasNaoConcluidas);
+  }
+}
+
+function verificaQualTarefaEh (listaTarefa) {
+  if (listaTarefa === tarefasConcluidas) {
+    estadoTarefa = 'tarefaConcluida';
+    tarefaFoiConcluida = true;
+  } else {
+    estadoTarefa = 'tarefaNaoConcluida';
+    tarefaFoiConcluida = false;
+  }
+}
+
+function concluirTarefa(indice) {
+  tarefasConcluidas.push(tarefasNaoConcluidas[indice]);
+  tarefasNaoConcluidas.splice(indice, 1);
+    imprimirLista(tarefasNaoConcluidas);
+}
+
+function imprimirLista(lista = []) {
+  divTarefas.innerHTML = '';
+  verificaQualTarefaEh(lista);
+  for (let i = 0; i < lista.length; i++) {
+    divTarefas.innerHTML += `<div class="${estadoTarefa}">
+            <h3>${lista[i]}</h3>
+            <button onclick="concluirTarefa(${i})">Concluir</button>
+            <button onclick="deletarTarefa(${i}, ${tarefaFoiConcluida})">Deletar</button>
           </div>`;
-        }
-    }
-}
-
-function limparTarefa(indice) {
-    vetor.splice(indice, 1);
-}
-
-function marcarConcluido(indice) {
-    let tarefa = document.querySelectorAll(".teste")[indice];
-    tarefa.classList.toggle("concluido");
+  }
 }
